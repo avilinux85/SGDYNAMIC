78a56c1f3fb116115552c6fae915caef31ebb97b
Avishek CHowdhury <avilinux85@gmail.com>
2023-06-17 16:15:28 +0530

    Dynamic_SG

---
+++ sg.tf
@@ -0,0 +1,36 @@
+variable "dyport" {
+  type    = list(number)
+  default = [22, 80, 443, 1521, 3306, 2049, 389, 25]
+}
+
+resource "aws_security_group" "allow_dynamic_ports" {
+  name        = "allow_dynamic_ports"
+  description = "Allow Dynamic Ports"
+
+
+  dynamic "ingress" {
+    for_each = var.dyport
+    iterator = port
+    content {
+      description = "Allow Dynamic Ports"
+      from_port   = port.value
+      to_port     = port.value
+      protocol    = "tcp"
+      cidr_blocks = ["0.0.0.0/0"]
+    }
+  }
+  dynamic "egress" {
+    for_each = var.dyport
+    iterator = port
+    content {
+      from_port   = port.value
+      to_port     = port.value
+      protocol    = "tcp"
+      cidr_blocks = ["0.0.0.0/0"]
+    }
+  }
+
+  tags = {
+    Name = "allow_dynamic_port"
+  }
+}
